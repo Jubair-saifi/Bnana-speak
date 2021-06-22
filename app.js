75daed29ccfc1fdf8b6159f980d7cb21059e1916
Jubair-saifi <jubair.saifi.cs.2017@miet.ac.in>
2021-06-22 13:54:40 +0530

    docs:final commit

--- app.js
+++ app.js
@@ -7,12 +7,21 @@ var outputDiv=document.querySelector("#output");
 //outputDiv.innerHTML="hello jubair";
 //console.log(outputDiv);
 
+var url="https://api.funtranslations.com/translate/mandalorian.json";
+
+function getTranselateUrl(text1){
+  return url+"?"+"text="+text1;
+}
+
 
 var btnTranselate=document.querySelector("#btn-transelate");
 btnTranselate.addEventListener("click",function addEventListener(){
   // console.log("clicked");
   // console.log(text.value);
-  outputDiv.innerHTML=text.value +"ansbnes";
-
-
-})
+  //outputDiv.innerHTML=text.value +"ansbnes";
+  var textinput=text.value;
+  fetch(getTranselateUrl(textinput)).then(Response =>Response.json()).then(json=>{
+    var translatedtext=json.contents.translated;
+    outputDiv.innerText=translatedtext;
+  });
+});
