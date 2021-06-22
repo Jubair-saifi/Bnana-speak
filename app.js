50211246a471674f7468d3eab99f30d7e2024346
Jubair-saifi <jubair.saifi.cs.2017@miet.ac.in>
2021-06-22 09:46:30 +0530

    input output processing is done

--- app.js
+++ app.js
@@ -3,11 +3,16 @@
 // alert("is this correct name?"+username);
 
 var text=document.querySelector("#text");
+var outputDiv=document.querySelector("#output");
+//outputDiv.innerHTML="hello jubair";
+//console.log(outputDiv);
 
 
 var btnTranselate=document.querySelector("#btn-transelate");
 btnTranselate.addEventListener("click",function addEventListener(){
-  console.log("clicked");
-  console.log(text.value);
+  // console.log("clicked");
+  // console.log(text.value);
+  outputDiv.innerHTML=text.value +"ansbnes";
+
 
 })
